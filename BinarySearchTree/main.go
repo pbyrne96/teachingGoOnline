a7c950881d067b6653048c20a2cbb4d9698e3c49
pbyrne96 <p.byrne@weareeight.com>
2022-10-13 08:49:19 +0100

    adding changes to binaryTree

--- BinarySearchTree/main.go
+++ BinarySearchTree/main.go
@@ -10,8 +10,8 @@ type Node struct {
 
 
 func (n *Node) Insert(newKey int) {
-	// takes a key as a param that is not already in the searchTree
-	if (!n.Search(newKey)) {
+	if (n.Search(newKey)) {
+		fmt.Println("already inserted");
 		return
 	}
 	if (n.key < newKey) {
@@ -34,20 +34,44 @@ func (n *Node) Insert(newKey int) {
 
 
 func (n *Node) Search(key int) bool {
-	if (n == nil || n.key == key) {
+
+	if (n == nil) {
 		return false
+	}
+	if (n.key == key) {
+		return true;
 	}
 	if (n.key < key) {
 		return n.rightNode.Search(key)
 	} else if n.key > key {
 		return n.leftNode.Search(key);
 	}
-	return true;
+	return false;
+}
+
+
+func (n *Node) SearchAndInsert(key int) {
+	// searches the binary Tree and returns the node where it should be the root of
+
 }
 
 func main() {
 	tree := &Node{key: 100};
+	tree.Insert(10);
+	tree.Insert(20);
+	tree.Insert(90);
+	tree.Insert(101);
+	tree.Insert(100);
+	tree.Insert(203);
+	tree.Insert(44);
+	tree.Insert(7);
+	tree.Insert(52);
+	tree.Insert(88);
+	tree.Insert(276);
 	tree.Insert(200);
 	tree.Insert(300);
+	tree.Insert(300);
 	fmt.Println(tree.leftNode, tree.rightNode, tree);
+	fmt.Println(tree.Search(76));
+	fmt.Println(tree.Search(203));
 }
