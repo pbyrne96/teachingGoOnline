a46ae84d3790aa1267a4716fb8ae19f1330a58df
pbyrne96 <p.byrne@weareeight.com>
2022-10-14 17:19:34 +0100

    adding quicker search funcion pattern

--- GreedyApproach/main.go
+++ GreedyApproach/main.go
@@ -23,16 +23,77 @@ func (g *Knapsack) Acceptable(check int) bool {
 }
 
 func (g *Knapsack) ProfitbyWeight(profit int, weight int) int {
-	return profit ^ weight
+	return weight / profit
 }
 
-func (g *Knapsack) Greedy(n int) []int {
-	// n marks the size or the capacity of the desired optimal output
 
-	cnt := 0;
-	for _, index := range *g.elements {
-		fmt.Println(index)
-		cnt++;
+type MaxAndIndex struct {
+	value int
+	index int
+}
+
+func (g* Knapsack) maxInArray() {
+
+}
+
+func (g* Knapsack) BinarySearch(arr []int, low int, high int ) MaxAndIndex {
+	returnValue := MaxAndIndex{value: 0 , index: -1}
+
+	for (low <= high) {
+		m := low + (high - low) / 2;
+		if(low==high) {
+			return MaxAndIndex{value: arr[low], index: low}
+		}
+		if ((high == low + 1) && ( arr[low] >= arr[high])) {
+			return MaxAndIndex{value: arr[low], index: low}
+		}
+		if ((high == low + 1) && (arr[low] < arr[high])) {
+			return MaxAndIndex{value: arr[high], index: high}
+		}
+		if ((arr[m] > arr[m+1]) && (arr[m] > arr[m-1])) {
+			return MaxAndIndex{value: arr[m], index: m}
+		}
+		if ((arr[m] > arr[m+1]) && (arr[m] < arr[m-1])) {
+			high = m - 1
+		} else {
+			low= m + 1
+		}
+	}
+	return returnValue
+}
+
+func (g *Knapsack) MaxAndIndex(ratio []int) MaxAndIndex {
+	return g.BinarySearch(ratio, 0, len(ratio)-1)
+}
+
+func (g *Knapsack) LinearMaxAndIndex(ratios []int)  MaxAndIndex{
+	// currently linear search -> could chunk into right and left and compare both results
+	returnValue := MaxAndIndex{value: 0, index: 0};
+
+	for index, value := range ratios {
+		if (value > returnValue.value) {
+			returnValue.value = value;
+			returnValue.index = index
+		}
+	}
+
+	return returnValue;
+}
+
+func (g *Knapsack) Greedy() []int {
+	// n marks the size or the capacity of the desired optimal output
+	weightAndIndex := []int{}
+	for index, value := range *g.elements {
+		currentRatio := g.ProfitbyWeight(value.profit, value.weight);
+		weightAndIndex[index] = currentRatio;
+	}
+	currentProfit := 0;
+	currentWeight := 0
+	for (currentProfit <= g.upperLim) {
+		currentMax := g.MaxAndIndex(weightAndIndex)
+		originalObject := (*g.elements)[currentMax.index]
+		currentWeight+=originalObject.weight;
+		currentProfit+=originalObject.profit;
 	}
 
 	return []int{0};
@@ -58,5 +119,5 @@ func main () {
 	initialKnapsack.elements = &allObject;
 	initialKnapsack.upperLim = 15
 
-	fmt.Println(initialKnapsack);
+	fmt.Println(initialKnapsack.Greedy());
 }
